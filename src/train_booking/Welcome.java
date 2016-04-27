015b1b612b582ad34d9578c4098ffd56d37cdd13
ottagit <ottachris13@yahoo.com>
2016-04-27 15:49:08 +0300

    changed developer name

--- src/train_booking/Welcome.java
+++ src/train_booking/Welcome.java
@@ -12,25 +12,25 @@ public class Welcome implements Runnable{
 	ImageIcon img;
 	Welcome()
 	{
-	img=new ImageIcon("indian-rail.jpg");
+	img=new ImageIcon("/logo_krc.png");
 	t=new Thread(this);
 	f=new JFrame("Welcome");
 	f.getContentPane().setLayout(null);
-	f.getContentPane().setBackground(Color.yellow);
+	f.setBackground(Color.lightGray);
 	l1=new JLabel("OnLine Reservation");
 	l1.setBounds(250,300,900,50);
-	l1.setFont(new Font("Courier new",Font.BOLD,60));
+	l1.setFont(new Font("Courier new",Font.BOLD,30));
 	l1.setForeground(Color.red);
-	l2=new JLabel(" _By Dushmanta Pradhan");
-	l2.setFont(new Font("Courier new",Font.BOLD,35));
+	l2=new JLabel(" _By Osawa C Otta");
+	l2.setFont(new Font("Courier new",Font.BOLD,20));
 	l2.setBounds(350,380,800,40);
 	l2.setForeground(Color.red);
 	l3=new JLabel(img);
 	l3.setBounds(0,0,1300,1000);
-	f.getContentPane().add(l1);
-	f.getContentPane().add(l2);
-	f.getContentPane().add(l3);
-	f.setSize(1300,1000);
+	f.add(l1);
+	f.add(l2);
+	f.add(l3);
+	f.setSize(800,700);
 	f.setVisible(true);
 	t.start();
 	}
